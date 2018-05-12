58d5ee05ef75f63c15ec216cf0923db9a700fb8e
caitlinmburke <38079583+caitlinmburke@users.noreply.github.com>
2018-05-12 22:39:25 +0000

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,14 +1,30 @@
 from picamera import PiCamera
 from time import *
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
 
 camera = PiCamera()
 
 camera.start_preview(fullscreen = False, window = (13,39, 645,480))
 try:
         while True:
-                sleep(1)
+                i=GPIO.input(17)
+                if i==0:                 #When output from motion sensor is LOW
+                        print "No intruders"
+                        camera.start_recording('/home/pi/Desktop/video.h264')
+                        sleep(0.1)
+                elif i==1:               #When output from motion sensor is HIGH
+                        print "Intruder detected"
+                        camera.stop_recording()
+                        sleep(0.1)
+                #sleep(1)
 
 except KeyboardInterrupt:
+        GPIO.cleanup()
+        camera.stop_recording()
         camera.stop_preview()
 
 
